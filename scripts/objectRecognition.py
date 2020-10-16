006d409b8437b357b91639f8f64519e0b5b5fb55
xch <335013385@qq.com>
2020-10-16 11:45:25 +0800

    update objectRecongintion

--- scripts/objectRecognition.py
+++ scripts/objectRecognition.py
@@ -1,28 +1,55 @@
 #!/usr/bin/env python
 #coding=utf-8
+import math
 import rospy
-from wrt.msg import GridMap
-from wrt.msg import Detection2DArray
+import base64
+from wrt.msg import Situation
+from wrt.msg import DetectingResult
+ 
 
-class ObjectRecongnition:
-    def __init__(self):
-        self.gridMap_sub = rospy.Subscriber('gridMap',GridMap,self.gridMap_callback,queue_size = 1)
-        self.detection_pub = rospy.Publisher('detection2DArray', Detection2DArray, queue_size = 1)
-    def gridMap_callback(self,gridMap_msg):
-        #处理数据
-        print ('objectRecongnition')
-        print(gridMap_msg.size)
-        print(gridMap_msg.topLeftLoc)
-        print(gridMap_msg.gridType)
-        print(gridMap_msg.gridInterval)
-        for stateInfo in gridMap_msg.cell:
-            print(stateInfo)
-        detection_msg = Detection2DArray()
-         #处理数据
+EARTH_REDIUS = 6378.137
+
+selfId = 1
+selfPos = Pos()
+pub = rospy.Publisher('objectTopic', DetectingResult)
+
+def rad(d):
+    return d * math.pi / 180.0
+
+def getDistance(pos1, pos2):
+    radLat1 = rad(pos1.latitude)
+    radLat2 = rad(pos2.latitude)
+
+    a = radLat1 - radLat2
+    b = rad(pos1.longtitude) - rad(pos2.longtitude)
+
+    s = 2 * math.asin(math.sqrt(math.pow(math.sin(a / 2), 2) + math.cos(radLat1) * math.cos(radLat2) * math.pow(math.sin(b / 2), 2)))
+    s = s * EARTH_REDIUS
+    return s
+
+def situationCallback(situationMsg):
+    wrtList = situationMsg.wrt_list
+    cargoBoatPos = Pos()
+    for wrt in wrtList:
+        if wrt.wrt_id == selfId:
+            selfPos = wrt.pos
+        if wrt.type == 0:
+            cargoBoatPos = wrt.pos
+    if (getDistance(selfPos, cargoBoatPos) <= 0.5):
+        pic = open("/home/xch/demo.jpg", "rb")
+        pic_base64 = base64.b64encode(pic.read())
+        pic.close()
+
+        
+        detectingResult = DetectingResult()
+        pub.publish(detectingResult)
+
+    else:
+        return
     
-        self.detection_pub.publish(detection_msg)
 
+rospy.init_node('objectRecognitionNode')
+ 
+sub = rospy.Subscriber('situationTopic', Situation, situationCallback)
 
-rospy.init_node('objRecognition_node')
-objectRecongnition = ObjectRecongnition()
 rospy.spin()
